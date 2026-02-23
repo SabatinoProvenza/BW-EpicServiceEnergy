d5628f49908645c7abd55b7f949eb2653c0aa34a
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-02-23 11:43:43 +0100

    creates project structure

---
+++ src/main/java/sabatinoprovenza/BW_EpicServiceEnergy/BwEpicServiceEnergyApplication.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BW_EpicServiceEnergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwEpicServiceEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwEpicServiceEnergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sabatinoprovenza/BW_EpicServiceEnergy/BwEpicServiceEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BW_EpicServiceEnergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwEpicServiceEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
