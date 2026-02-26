f5aa4554e534b469dda5fb722ca5a0bbe5d3f7fc
Filippo-Timo <filtimo2002@gmail.com>
2026-02-26 15:17:24 +0100

    Deletes comments

--- src/main/java/sabatinoprovenza/BW_EpicServiceEnergy/security/SecurityConfig.java
+++ src/main/java/sabatinoprovenza/BW_EpicServiceEnergy/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
 
-        httpSecurity.cors(Customizer.withDefaults()); // TODO: OBBLIGATORIA se vogliamo usare la configurazione per il frontend (che abbiamo scritto nell'ultimo @Bean in fondo)
+        httpSecurity.cors(Customizer.withDefaults());
 
         return httpSecurity.build();
 
@@ -43,17 +43,12 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration(); // Questo è l'oggetto che creo per la configurazione
+        CorsConfiguration configuration = new CorsConfiguration();
 
-        // Nella prossima riga (riga 93) definisco una WHITELIST di uno o più indirizzi FRONTEND che voglio possano accedere a questo backend senza i problemi di CORS
-        // Volendo ma rischioso si potrebbe anche mettere '*' però questo permetterebbe l'accesso a tutti (utile solo nel caso di API pubbliche)
-        configuration.setAllowedOrigins(List.of("http://localhost:5174", "https://exampleFronrend.com")); // List.of -> Lista di stringhe
-        // configuration.setAllowedOrigins(List.of("*")); <--- Questo per Api pubbliche perché dà accesso a tutti a questo Backend (rischioso in caso di Api non pubbliche)
-
-        configuration.setAllowedMethods(List.of("*")); // Qui inserisco i metodi a cui si può avere accesso
+        configuration.setAllowedOrigins(List.of("http://localhost:5174", "https://exampleFronrend.com"));
+        configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
 
-        // E infine qui sotto lo impostiamo su di un URL
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
